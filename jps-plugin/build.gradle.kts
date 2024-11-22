aa9b5818419aae362b3537ddb538c7773c8bfe8d
Barış Yıldırım <25794892+barisyild@users.noreply.github.com>
2024-11-22 19:10:29 +0300

    Update build.gradle.kts

--- jps-plugin/build.gradle.kts
+++ jps-plugin/build.gradle.kts
@@ -4,3 +4,7 @@ plugins {
 dependencies {
   implementation(project(":common"))
 }
+java {
+    sourceCompatibility = JavaVersion.VERSION_11  // Örneğin Java 11
+    targetCompatibility = JavaVersion.VERSION_11  // Örneğin Java 11
+}
