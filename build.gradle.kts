3f1734a54d9734941729616e3603de5d418a448e
Barış Yıldırım <25794892+barisyild@users.noreply.github.com>
2024-11-22 19:36:55 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_17
     }
 }
