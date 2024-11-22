4b2e64034abd711ecf74b8aae85103fa4f5bcafe
Barış Yıldırım <25794892+barisyild@users.noreply.github.com>
2024-11-22 19:39:52 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,7 +99,7 @@ allprojects {
 
     java {
         sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
